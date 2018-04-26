de5ce4966b9cb8c8fa1f4b716c8b6f6be66d4b79
Sun Davey <sundavey@hotmail.com>
2018-04-26 14:44:30 -0700

    created visual placeholder for userdashboard

--- Controllers/TravelController.cs
+++ Controllers/TravelController.cs
@@ -29,7 +29,19 @@ namespace Hitchhikers.Controllers
             }
             // ViewBag.state = state;
             // ViewBag.Email = HttpContext.Session.GetString("email");
-            // ViewBag.CurrentUserID = (int)HttpContext.Session.GetInt32("CurrentUserID");
+            ViewBag.CurrentUserID = (int)HttpContext.Session.GetInt32("CurrentUserID");
+            var currentUser = _dbcontext.Users.Where(User=>User.Userid == (int)HttpContext.Session.GetInt32("CurrentUserID"))
+                                    .Include(pic=>pic.Uploaded).ToList();
+            // var userstates = _dbcontext.Users.Where(User=>User.Userid == (int)HttpContext.Session.GetInt32("CurrentUserID"))
+            //             .Include(pic=>pic.Uploaded.GroupBy(p=>p.States)).ToList();
+            //             foreach(var item in userstates){
+            //                 System.Console.WriteLine(item.Uploaded.Count());
+                            
+            //             }
+            var uploaded = _dbcontext.Pictures.Where(user=>user.UploaderId == (int)HttpContext.Session.GetInt32("CurrentUserID")).GroupBy(s=>s.States).ToList();
+            var alluploaded = _dbcontext.Pictures.Where(user=>user.UploaderId == (int)HttpContext.Session.GetInt32("CurrentUserID")).ToList();
+            ViewBag.CurrentUser = currentUser;
+            ViewBag.AllUploaded = alluploaded;
             return View("Dashboard");
         }
 
@@ -67,6 +79,9 @@ namespace Hitchhikers.Controllers
                 _dbcontext.SaveChanges();
                 return View("Dashboard");
             }
+            List<string> StateList = new List<string>
+            { "AL", "AK", "AS", "AZ", "AR", "CA", "CO", "CT", "DE", "DC","FM", "FL","GA", "GU","HI","ID","IL", "IN", "IA","KS","KY","LA","ME", "MH", "MD", "MA", "MI","MN","MS","MO", "MT", "NE","NV","NH","NJ", "NM", "NY", "NC", "ND","MP","OH","OK", "OR",  "PW", "PA","PR","RI", "SC", "SD", "TN","TX","UT","VT", "VI", "VA", "WA", "WV", "WI","WY"};
+            ViewBag.all_states = StateList;
             return View("Create");
         }
 
@@ -74,7 +89,7 @@ namespace Hitchhikers.Controllers
         [Route("ViewPicture")]
         public IActionResult ViewPicture()
         {
-            return View("");
+            return View("ViewPicture");
         }
 
         [HttpGet]
